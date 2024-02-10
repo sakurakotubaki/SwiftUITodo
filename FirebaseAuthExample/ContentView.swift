2a605bac91a1ffb440e02790279954594d5ac03c
JunichiHashimoto <gitonly543@gmail.com>
2024-02-10 15:29:27 +0900

    Initial Commit

---
+++ FirebaseAuthExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseAuthExample
+//
+//  Created by 橋本純一 on 2024/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseAuthExample/FirebaseAuthExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthExampleApp.swift
+//  FirebaseAuthExample
+//
+//  Created by 橋本純一 on 2024/02/10.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
